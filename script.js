e84a167c8b9ae84826c7f51abb795c06a4e4bbd5
Im-Hassan-wd <alhassansahih@gmail.com>
2021-08-20 21:17:37 +0100

    functionality

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const skate = document.querySelector('.skate');
+const skateSection = document.querySelector('.skate');
+const skateboarder = document.querySelector('.skate-boi');
 
-setTimeout(() => {
-  skate.classList.add('active');
-}, 30000);
+setTimeout(()=>{
+  skateSection.classList.add('active');
+},13000);
+
+// skate animation
+const showSkater = () => {
+
+  const sectionPosition = skateSection.getBoundingClientRect().top;
+  const screenPosition = innerHeight / 1.2;
+  // console.log(screenPosition, sectionPosition)
+
+  if(sectionPosition < screenPosition){
+    skateboarder.style.animation = 'skate 8s linear both 3';
+  }
+
+}
+// scrollAppear()
+window.addEventListener('scroll', showSkater)
