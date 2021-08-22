1bd1f34f0eb254304750f09b069657d63c35b408
Im-Hassan-wd <alhassansahih@gmail.com>
2021-08-22 22:19:53 +0100

    carousel slider

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const burger = document.querySelector('header svg');
 const nav = document.querySelector('header ul');
 const skateBtn = document.querySelector('.title a'),
 skateMe = document.querySelector('.title p');
-
 // Navigation menu
 burger.addEventListener('click',() => {
   nav.classList.toggle('active');
@@ -30,4 +29,58 @@ const showSkater = () => {
 
 }
 // scrollAppear()
-window.addEventListener('scroll', showSkater)
+window.addEventListener('scroll', showSkater);
+
+//image slide PROJECTS
+const slider = document.querySelector('.project-slide');
+const carouselImages = document.querySelectorAll('.project-slide img');
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+//Counter
+let counter = 1;
+const size = 700;
+
+
+slider.style.transform = 'translateX(' + (-size*counter) + 'px)';
+
+nextBtn.addEventListener('click',()=>{
+    if(counter >= carouselImages.length - 1) return;
+    slider.style.transition = 'transform 0.5s ease-in-out';
+    counter++;
+    slider.style.transform = 'translateX(' + (-size*counter) + 'px)';
+});
+
+prevBtn.addEventListener('click',()=>{
+    if(counter <= 0) return;
+    slider.style.transition = 'transform 0.5s ease-in-out';
+    counter--;
+    slider.style.transform = 'translateX(' + (-size*counter) + 'px)';
+});
+
+
+slider.addEventListener('transitionend',()=>{
+    if(carouselImages[counter].id === 'lastClone'){
+        slider.style.transition = 'none';
+        counter = carouselImages.length -2;
+        slider.style.transform = 'translateX(' + (-size*counter) + 'px)';
+    }
+    if(carouselImages[counter].id === 'firstClone'){
+        slider.style.transition = 'none';
+        counter = carouselImages.length - counter;
+        slider.style.transform = 'translateX(' + (-size*counter) + 'px)';
+
+    }
+});
+
+console.log(carouselImages.length);
+
+
+
+
+
+
+
+
+
+// shjdk
